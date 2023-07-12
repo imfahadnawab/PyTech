4c576019a7a861641cd50ac7572a8d0a20280daa
Fahad <67440860+imfahadnawab@users.noreply.github.com>
2023-07-13 05:08:54 +0530

    Update settings.py
    Signed-off-by: Fahad <67440860+imfahadnawab@users.noreply.github.com>

--- PyTech/settings.py
+++ PyTech/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*2yzy8j=p*ch5wlwj^odn8flf=+@u59lqdfoo2p*^22$1%a56n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -129,11 +128,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join[BASE_DIR,'staticfiles']
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR,"static")
+]
 
-#STATICFILES_DIRS = [
- #   os.path.join(BASE_DIR,"static")
-#]
 
-
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
